97686d3f21b15849cabee0fc7c1eee5b54d54e7e
Natan - Laptop <natankolodziej@yahoo.com>
2023-06-28 10:12:00 +0100

    added ProjectSummary page, major changes in the way the dropdown menu works

--- indexjs.js
+++ indexjs.js
@@ -1,28 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var button = document.getElementById("ProfessionalBackgroundButton");
-  var dropdown = document.getElementById("DropDownDiv");
-  var options = dropdown.getElementsByTagName("option");
+// Toggle the dropdown menu on mobile
+function toggleDropdown() {
+  var dropdownContent = document.querySelector('#ProfessionalBackgroundLinks');
+  dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+}
 
-  // Function to toggle the visibility of the dropdown
-  function toggleDropdown() {
-    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-  }
+// Attach event listener to the dropdown button
+var dropdownButton = document.querySelector('#ProfessionalBackgroundButton');
+dropdownButton.addEventListener('click', toggleDropdown);
 
-  // Event listener to trigger the toggleDropdown function when the button is clicked
-  button.addEventListener("click", toggleDropdown);
 
-  for (var i = 0; i < options.length; i++) {
-    options[i].addEventListener("click", handleOptionSelection);
-    options[i].addEventListener("change", handleOptionSelection);
-  }
-
-  function handleOptionSelection() {
-    var selectedValue = this.value;
-    // Redirect the user to the appropriate page based on the selected option
-    if (selectedValue === "SkillsSummary") {
-      window.location.href = "SummaryOfSkills.html"; // Keep the same URL
-    } else if (selectedValue === "CV") {
-      window.location.href = "CV.html"; // Keep the same URL
-    }
-  }
 });
+
